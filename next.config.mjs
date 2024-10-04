7b200cd42b79c5fdaf3eba65b2b3f2db203f84ad
SalehZababneh <saleh.zabadneh@gmail.com>
2024-10-04 23:51:31 +0300

    8

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   productionBrowserSourceMaps: true,
   images: {
     remotePatterns: [
@@ -10,6 +11,45 @@ const nextConfig = {
         pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/**`,
       },
     ],
+  },
+  // Add the following configurations:
+  poweredByHeader: false,
+  compress: true,
+  i18n: {
+    locales: ["en", "ar"],
+    defaultLocale: "en",
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Custom webpack configurations can be added here
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
   },
 };
 
