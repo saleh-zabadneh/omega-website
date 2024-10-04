45a35434bd2383b268a7825795033391d599380c
SalehZababneh <saleh.zabadneh@gmail.com>
2024-10-04 23:28:51 +0300

    6

--- config/config-helper.ts
+++
@@ -1,9 +0,0 @@
-export function safelyIterateConfig(config: unknown): void {
-  if (config && typeof config === "object" && Symbol.iterator in config) {
-    for (const item of config as Iterable<unknown>) {
-      console.log(item);
-    }
-  } else {
-    console.error("Config is not iterable:", config);
-  }
-}
